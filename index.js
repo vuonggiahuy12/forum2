ec67825a1a469c553e0faf4ed53ec54d03c9b8a9
vuonggiahuy12 <76721193+vuonggiahuy12@users.noreply.github.com>
2021-01-02 14:40:03 +0700

    doan

---
+++ index.js
@@ -0,0 +1,20 @@
+$('.nav-tabs a').click(function(){
+  $(this).tab('show');
+})
+
+// Select tab by name
+$('.nav-tabs a[href="#home"]').tab('show')
+
+// Select first tab
+$('.nav-tabs a:first').tab('show')
+
+// Select last tab
+$('.nav-tabs a:last').tab('show')
+
+// Select fourth tab (zero-based)
+$('.nav-tabs li:eq(3) a').tab('show')
+$(document).ready(function() {
+  $('#Carousel').carousel({
+      interval: 5000
+  })
+});
